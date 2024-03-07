3967551f30e77984bc87f374ee9e57fbfffe6499
Moises E Jaramillo <moisesj@outlook.com>
2024-03-06 22:36:31 -0500

    Starting to expose ServiceClients

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,8 @@
 // Expose all public types and functionality here
 export * from "./ucw-client.js";
 export * from "./config.js";
+//export * from "./serviceClients/akoyaClient/index.js";
+//export * from "./serviceClients/finicityClient/index.js";
+//export * from "./serviceClients/mxClient/index.js";
+export * from "./serviceClients/sophtronClient/index.js";
+export * from "./provider-configuration.js";
